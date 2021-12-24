d6ec9f5babbb44e426ee5b128c00530abfbe0b16
Irina Zaborskaya <zaborskayaie@gmail.com>
2021-12-24 22:17:00 +0500

    task_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,23 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [{product.title, count}], order: {address, sum}, client: {name, phone}};
+  let data = {goods: [], order: {}};
 
-    let countOfGoods = goods.length;
+  let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+  for (let i = 0; i < countOfGoods; i += 1) {
+      const product = {
+        title: goods[i].title,
+        count: goods[i].count
+      }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+      data.goods.push(product);
+  }
 
-    data.client = 'Иван';
+  data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+  data.order.sum = sum;
 
-    let jsonData = JSON.stringify(data);
+  data.client = `${name} ${phone}`;
 
-    return jsonData;
+return JSON.stringify({data});
 }
